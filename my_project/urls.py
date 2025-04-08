71355c8e711224f313843b6dfe83031219b9e71f
sims-bit <xfiqoe46@students.codeinstitute.net>
2025-04-08 17:21:39 +0100

    Install whitenoise package

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 from hello_world import views as index_views
 from about import views as about_views 
 
-urlpatterns = [
-    path('', index_views.index, name='index'),
+urlpatterns = [git 
+    path('hello/', index_views.index, name='index'),
     path('admin/', admin.site.urls),
     path('about/', about_views.about_me, name='about'),
 ]
